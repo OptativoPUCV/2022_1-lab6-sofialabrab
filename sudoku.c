3b260adc1dd7f4930096e216f77bef61c67ecb34
sofialabrab1 <>
2022-05-12 15:23:50 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -49,8 +49,33 @@ int is_valid(Node* n){
 }
 
 
-List* get_adj_nodes(Node* n){
-    List* list=createList();
+List* get_adj_nodes(Node* n)
+{
+  List* list=createList();
+  int i,j,k;
+  for(i = 0 ; i < 9; i++)
+  {
+    for(j = 0; j < 9; j++)
+    {
+      if(n->sudo[i][j] == 0)
+      {
+        for(k = 1; k < 10; k++)
+        {
+          n->sudo[i][j] = k;
+          if(is_valid(n))
+          {
+            Node *adj = copy(n);
+            pushBack(list,adj);
+          }   
+        }
+        n->sudo[i][j] = 0;
+        return list;
+      
+      }
+        
+    }
+      
+  }
     return list;
 }
 
