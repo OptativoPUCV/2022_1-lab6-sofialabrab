0462673f8c1494df497b6d012b61468e2eaa363b
sofialabrab1 <>
2022-05-13 15:24:18 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -149,7 +149,7 @@ int is_final(Node* n)
     printf("Final");
     return 2;
   } 
-  printf(n->sudo[0][2]);
+  printf("%i", n->sudo[0][2]);
   for(int i = 0; i < 9; i++)
   {
     for(int j = 0; j < 9; j++)
